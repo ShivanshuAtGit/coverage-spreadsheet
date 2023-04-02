98618838c2915764ae6375da661a55d1e8f964ba
Shivanshu Srivastava <yashusrivastavab1231@gmail.com>
2023-04-02 23:39:46 +0530

    adding dom parser

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const DomParser = require('dom-parser');
+const parser = new DomParser();
 
 (async () => {
   try {
     core.notice("Calling our action --> spreadsheet");
     const report = core.getInput("report");
-    console.log(`------>  ${report}!`);
+    const dom = parser.parseFromString(report);
+    console.log(dom)
+
+    console.log(dom.getElementsByTagName('div'));
   } catch (err) {
     core.setFailed(err.message);
   }
