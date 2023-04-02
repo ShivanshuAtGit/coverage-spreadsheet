d79b5404a1ac48a789c23f435082f093004b94a7
Shivanshu Srivastava <yashusrivastavab1231@gmail.com>
2023-04-02 23:12:24 +0530

    adding report and accepting as input

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const github = require("@actions/github");
 (async () => {
   try {
     core.notice("Calling our action --> spreadsheet");
+    const report = core.getInput("report");
+    console.log(`------>  ${report}!`);
   } catch (err) {
     core.setFailed(err.message);
   }
